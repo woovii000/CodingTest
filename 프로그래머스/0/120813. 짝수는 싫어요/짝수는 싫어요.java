6ddbee9dfac1569b67a6ae31849c658621383f38
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-07 23:37:22 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,10 +1,11 @@
 class Solution {
     public int[] solution(int n) {
-        int idx =  (n%2==0) ? n/2 : n/2+1 ; //짝.홀 나누고 길이 확인*
-        int[] answer = new int[idx];
-        for(int i = 0; i <idx; i++){
-                answer[i] += (2*i+1);
-        } 
-        return answer; // 일정갯수 넣기 -> 길이 확인
+        //홀짝에 따른 크기
+        int size = n%2 == 0 ? n/2 : n/2+1 ;
+        int[] answer = new int[size];
+        for(int i=0; i<size; i++){
+            answer[i] = 2*i+1;
+        }
+        return answer;
     }
 }
