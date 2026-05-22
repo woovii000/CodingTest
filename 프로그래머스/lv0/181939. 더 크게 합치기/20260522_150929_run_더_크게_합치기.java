78e48bba3f1d78f55775ab9f2793d7e5705380de
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-22 15:09:31 +0900

    [ALG] run | 프로그래머스 | lv0 | 더 크게 합치기 | Java | 11번째 시도

---
+++ 프로그래머스/lv0/181939. 더 크게 합치기/20260522_150929_run_더_크게_합치기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b) {
+        String A = String.valueOf(a);
+        String B = String.valueOf(b);
+        // int c = Integer.parseInt(A+B)
+        // d 
+        if( Integer.parseInt(A+B) >= Integer.parseInt(B+A)){ return Integer.parseInt(A+B); }
+        else { return Integer.parseInt(B+A);}
+    }
+}
