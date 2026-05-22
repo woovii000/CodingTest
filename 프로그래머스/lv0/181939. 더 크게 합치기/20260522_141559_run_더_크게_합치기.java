e9b25656a6253248822ca25fc6cc030b7fa3e336
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-22 14:16:00 +0900

    [ALG] run | 프로그래머스 | lv0 | 더 크게 합치기 | Java | 8번째 시도

---
+++ 프로그래머스/lv0/181939. 더 크게 합치기/20260522_141559_run_더_크게_합치기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b) {
+        String C = "";
+        String D = "";
+        C += ;
+        D += ;
+        if(Integer.parseInt(C) >= Integer.parseInt(D)){ return Integer.parseInt(D); }
+        else { return Integer.parseInt(D);}
+    }
+}
