f39fb4563843066800fba8fc30a789e7046f15ac
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-27 17:08:14 +0900

    [ALG] run | 프로그래머스 | lv0 | 등차수열의 특정한 항만 더하기 | Python3 | 3번째 시도

---
+++ 프로그래머스/lv0/181931. 등차수열의 특정한 항만 더하기/20260527_170813_run_등차수열의_특정한_항만_더하기.py
@@ -0,0 +1,9 @@
+def solution(a, d, included):
+    # a+(n-1)b
+    sum = 0
+    for i in range(len(included)):
+        if included[i]:
+            sum += a+i*d
+        else:
+            sum
+    return sum
