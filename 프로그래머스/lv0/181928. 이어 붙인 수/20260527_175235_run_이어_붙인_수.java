b96aa4b96b916946a79db635b5bd5368bc8efb44
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-27 17:52:36 +0900

    [ALG] run | 프로그래머스 | lv0 | 이어 붙인 수 | Java | 7번째 시도

---
+++ 프로그래머스/lv0/181928. 이어 붙인 수/20260527_175235_run_이어_붙인_수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+        String answer = "";
+        for(int i=0; i<num_list.length; i++){
+            if(i%2==0){
+                even += (char)num_list[i];
+            }else{
+                odd += (char)num_list[i];
+            }   
+        }
+        answer = even+odd;
+        return (int)answer;
+    }
+}
