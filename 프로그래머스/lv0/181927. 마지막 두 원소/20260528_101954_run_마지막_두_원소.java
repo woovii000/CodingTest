d545437940b3cddb1f45f254e8b7c129bd1ed23c
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-28 10:19:56 +0900

    [ALG] run | 프로그래머스 | lv0 | 마지막 두 원소 | Java | 4번째 시도

---
+++ 프로그래머스/lv0/181927. 마지막 두 원소/20260528_101954_run_마지막_두_원소.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int l = num_list.length;
+        List<Integer> list = new ArrayList<>();
+        for(i : num_list){
+            list.add(i);
+        }
+        if(num_list[l-1]>num_list[l-2]){
+            list.add(num_list[l-1]-num_list[l-2]);
+        }else{
+            list.add(num_list[l-1]*2);
+        }
+        return list;
+    }
+}
