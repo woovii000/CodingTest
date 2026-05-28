7887171fe5a2641c183fa029c33d3c925bec0d09
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-28 17:52:03 +0900

    [ALG] run | 프로그래머스 | lv0 | 수열과 구간 쿼리 2 | Python3 | 2번째 시도

---
+++ 프로그래머스/lv0/181923. 수열과 구간 쿼리 2/20260528_175202_run_수열과_구간_쿼리_2.py
@@ -0,0 +1,9 @@
+def solution(arr, queries):
+    answer = []
+    for i in range(len(queries)):
+        #queries[i][0]<= i <=queries[i][1]
+        if queries[i][2] < min(arr[queries[i][0]:queries[i][1]+1]):
+            answer.append(min(arr))
+    if "".join(answer) :
+        return "".join(answer)
+    return -1;
