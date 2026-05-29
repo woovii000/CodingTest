91aa64d0b976417fa972d062fcd3c1a9af83d432
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-29 14:10:12 +0900

    [ALG] run | 프로그래머스 | lv0 | 배열 만들기 2 | Java | 17번째 시도

---
+++ 프로그래머스/lv0/181921. 배열 만들기 2/20260529_141011_run_배열_만들기_2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> num = new ArrayList<>();
+        for(int i=l; i<=r; i++){
+            if(String.valueOf(i).contains("0" || "5")){
+                
+            }
+        }
+        int[] answer = new int[num.size];
+        return answer;
+    }
+}
