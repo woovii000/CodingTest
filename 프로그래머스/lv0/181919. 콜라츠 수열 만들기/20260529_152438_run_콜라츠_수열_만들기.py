75c3c558266a0b9d1175c5d97118408cb2c13705
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-05-29 15:24:39 +0900

    [ALG] run | 프로그래머스 | lv0 | 콜라츠 수열 만들기 | Python3 | 2번째 시도

---
+++ 프로그래머스/lv0/181919. 콜라츠 수열 만들기/20260529_152438_run_콜라츠_수열_만들기.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    while n!=1:
+        if n%2==0:
+            n /= 2
+            answer.append(n)
+        else:
+            n = 3*n+1
+            answer.append(n)
+    return answer
