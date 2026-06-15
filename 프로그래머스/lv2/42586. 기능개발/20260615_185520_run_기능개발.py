5122f9e65944474bc99b36c4468533670184d2f5
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-15 18:55:22 +0900

    [ALG] run | 프로그래머스 | lv2 | 기능개발 | Python3 | 2번째 시도

---
+++ 프로그래머스/lv2/42586. 기능개발/20260615_185520_run_기능개발.py
@@ -0,0 +1,17 @@
+def solution(progresses, speeds):
+    answer = []
+    n = 0
+    a = 0
+    while a != len(progresses):
+        for i in range(len(progresses[:])):
+            progresses[i] += speeds[i]
+        for i in range(a,len(progresses[:])):
+            if progresses[i] <100:
+                continue
+            elif progresses[i] >= 100:
+                n += 1
+                a += 1
+        if n:
+            answer.append(n)
+        n=0
+    return answer
