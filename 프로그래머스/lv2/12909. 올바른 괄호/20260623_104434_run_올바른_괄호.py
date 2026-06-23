05ad4e6bb22e29d7df9d6ed9536f936e42ab2180
woovii000 <134402126+woovii000@users.noreply.github.com>
2026-06-23 10:44:35 +0900

    [ALG] run | 프로그래머스 | lv2 | 올바른 괄호 | Python3 | 4번째 시도

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/20260623_104434_run_올바른_괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    # ( 만나면 스택 )만나면 지우면서 (팝
+    # ) 시작 -> false
+    answer = []
+    for i in range(len(s)):
+        if s[i] == ")":
+            return False
+        elif s[i] == "(":
+            answer.append(s[i])
+        elif s[i] == ")" and not len(answer):
+            answer.pop()
+    return not answer
