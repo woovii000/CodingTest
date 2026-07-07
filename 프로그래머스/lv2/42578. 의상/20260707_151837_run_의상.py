f6020d791f7028520f9cfc0bde8c3fb70655a3a9
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-07 15:18:38 +0900

    [ALG] run | 프로그래머스 | lv2 | 의상 | Python3 | 18번째 시도

---
+++ 프로그래머스/lv2/42578. 의상/20260707_151837_run_의상.py
@@ -0,0 +1,19 @@
+def solution(clothes):
+    # clothes[i][1] 을 키로 해서 값이 있으면 +1 없으면 key value 추가
+    # {k1: a, k2: b, ...}
+    categories = {}
+    for i in range(len(clothes)):
+        if clothes[i][1] in categories:
+            categories[clothes[i][1]] += 1
+        else:
+            categories[clothes[i][1]] = 1
+    # 경우의 수 계산**
+    answer = 1
+    for ch in categories:
+        answer *= categories[ch]+1
+    # 키 1개
+    if len(categories) == 1:
+        return answer-1
+    # 안 입는 경우(1개) 뺴기
+    else: 
+        return answer-1
