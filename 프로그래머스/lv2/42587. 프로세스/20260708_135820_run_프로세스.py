74e6d8cb12f5d63c35815943cf44f02ab83c17ac
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-08 13:58:21 +0900

    [ALG] run | 프로그래머스 | lv2 | 프로세스 | Python3 | 5번째 시도

---
+++ 프로그래머스/lv2/42587. 프로세스/20260708_135820_run_프로세스.py
@@ -0,0 +1,11 @@
+def solution(priorities, location):
+    # for 
+    # idx - location
+    # priorities[idx]
+    # 주의! idx 앞 부터 빼는게 아닌 뒤부터 뺸다. pop()은 뒤에서부터 뺀다
+    answer = 0
+    while len(priorities[:]) != 0:
+        if location == priorities.index(max(priorities)):
+            return answer+1
+        priorities[:].pop(max(priorities))
+        answer += 1
