e3e5c355e50dd40a98756a7239fc165d06c54ee1
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-08 17:46:44 +0900

    [ALG] run | 프로그래머스 | lv2 | 프로세스 | Python3 | 8번째 시도

---
+++ 프로그래머스/lv2/42587. 프로세스/20260708_174643_run_프로세스.py
@@ -0,0 +1,26 @@
+def solution(priorities, location):
+                
+# 흐름 - 맨 앞에서 하나씩 꺼내며 뒤로 보내는 과정
+# while
+# [(2,0), (1,1), (3,2), (2,3)] 로 (우선순위, 원래 인덱스) 만들기 -> enumerate()
+# [][0] 가 더 큰게 있다? -> pop(idx) + append() -> for문 직접 , max(x for x in __)
+# 제일 크다? pop(idx) + count++
+# [][1] == location -> return
+    
+    queue = []
+    count = 0
+    for index, value in enumerate(priorities):
+        queue.append((value,index))
+    m = max(x for x in queue) # m == m[0]        
+    while queue:
+        cur = queue[0] # (a,b)
+
+        if cur[0] >= m:
+            queue.pop(cur)
+            count += 1
+        else:
+            queue.pop(cur)
+            append(cur)
+        if cur[1] == location:
+            return count
+        
