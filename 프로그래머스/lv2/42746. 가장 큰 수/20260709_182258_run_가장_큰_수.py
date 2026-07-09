1e04ce605cafbeeaf811f635dbaf360f7c49a429
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-09 18:22:59 +0900

    [ALG] run | 프로그래머스 | lv2 | 가장 큰 수 | Python3 | 7번째 시도

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/20260709_182258_run_가장_큰_수.py
@@ -0,0 +1,16 @@
+def solution(numbers):
+    # 흐름 numbers 속 숫자 -> 문자 변환+케이스별로 이어붙이기-> 숫자 변환 ->str(max(케이스)) 반환
+    # 위에는 말 그대로 했을 떄, 낭비
+    
+    # 핵심 - 맨 앞 자리가 크면 됨 -> a+b b+a 일 때 앞자리 비교
+    # 힌트 - sort(key=lambda x: x*3, reverse=True)**
+    for i in range(len((numbers))): # [] -> 문자열 변환
+        numbers[i] = str(numbers[i])
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    answer = ''
+    for ch in numbers:
+        answer += ch
+        
+    if int(answer) == 0:
+        return '0'
+    return answer
