b8d164c0a142c3e3a52a569e426419b151bdd639
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-10 17:42:23 +0900

    [ALG] run | 프로그래머스 | lv2 | 카펫 | Python3 | 32번째 시도

---
+++ 프로그래머스/lv2/42842. 카펫/20260710_174222_run_카펫.py
@@ -0,0 +1,9 @@
+def solution(brown, yellow):
+    # g*s 형태(g >= s) -> 조합 찾기
+    # 카펫 패턴 - i**2 , n*m
+    # b, y
+    carpet = brown + yellow
+    for se in range(2,carpet):
+        for ga in range(2,carpet):
+            if ga*se == carpet and ga-se >=0:
+                return [ga,se]
