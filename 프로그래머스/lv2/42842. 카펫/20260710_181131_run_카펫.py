da6f06c6beca744d8a359b1aa01645d9e3b3f7cd
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-10 18:11:32 +0900

    [ALG] run | 프로그래머스 | lv2 | 카펫 | Python3 | 46번째 시도

---
+++ 프로그래머스/lv2/42842. 카펫/20260710_181131_run_카펫.py
@@ -0,0 +1,16 @@
+def solution(brown, yellow):
+    # g*s 형태(g >= s) -> 조합 찾기
+    # 카펫 패턴 - i**2 , n*m -> 무의미
+    carpet = brown + yellow
+    
+    pattern = []
+    # 타일 수 패턴
+    for i in range(1,carpet):
+        if carpet//i >= i:
+            pattern.append((i,carpet//i))
+    
+    # 색 조건
+    for se, ga in answer:
+        if (ga-2)*(se-2)==yellow:
+            return [ga, se]
+    # => 약수 조합(ga * se == carpet) 찾기
