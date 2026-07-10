ca7b0fc215ac5d8f5422ca98ece8ec94c945b799
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-10 13:57:39 +0900

    [ALG] run | 프로그래머스 | lv2 | 카펫 | Python3 | 4번째 시도

---
+++ 프로그래머스/lv2/42842. 카펫/20260710_135738_run_카펫.py
@@ -0,0 +1,7 @@
+def solution(brown, yellow):
+    # n*m 형태 -> 조합 찾기?
+    carpet = brown+yellow
+    for i in range(carpet/2):
+        for j in range(i+1,carpet/2):
+            if i*j == carpet and j-i>= 2:
+                return [i,j]
