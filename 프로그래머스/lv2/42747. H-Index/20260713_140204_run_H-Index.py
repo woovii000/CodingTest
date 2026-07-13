598d83420fbf277c45e90c157041f66f500c131c
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-13 14:02:06 +0900

    [ALG] run | 프로그래머스 | lv2 | H-Index | Python3 | 1번째 시도

---
+++ 프로그래머스/lv2/42747. H-Index/20260713_140204_run_H-Index.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    citations.sort()
+    # h보다 큰 원소가 h개 있어야 함. 
+    for i in range(len(citations)):
+        # i 보다 큰 원소가 citantions에 얼마나 있는지 확인
+        for j in range(len(citations)):
+            if i <= citations[j]:
+                answer += 1
+        if i == answer:
+            return answer
