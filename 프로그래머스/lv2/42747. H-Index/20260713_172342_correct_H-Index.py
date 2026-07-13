898e39d6987dfecaba330515438e4f61f95e3fbb
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-13 17:23:44 +0900

    [ALG] correct | 프로그래머스 | lv2 | H-Index | Python3 | 22번째 시도

---
+++ 프로그래머스/lv2/42747. H-Index/20260713_172342_correct_H-Index.py
@@ -0,0 +1,15 @@
+def solution(citations):
+    citations.sort()
+    # i회 이상 인용된 논문이 i편 이상 -> 그 중 max
+    answer = 0
+    for i in range(1,len(citations)+1): #1~citations까지
+        count = 0
+        # i 보다 큰 원소가 citantions에 얼마나 있는지 확인
+        for j in range(len(citations)): # 0~idx
+            if i <= citations[j]: #
+                count += 1 # 인용 i회한 논문의 수 i편
+        if i <= count: # i == i회 인용한 논문의 수 => 중에 최대 -> i<=count
+            answer = i
+    if i == None: 
+        return 0
+    return answer
