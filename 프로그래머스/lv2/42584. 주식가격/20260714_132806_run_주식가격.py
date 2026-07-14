cc08789f231f3851042bc7f7815eaeff2b2b0b8e
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-14 13:28:08 +0900

    [ALG] run | 프로그래머스 | lv2 | 주식가격 | Python3 | 11번째 시도

---
+++ 프로그래머스/lv2/42584. 주식가격/20260714_132806_run_주식가격.py
@@ -0,0 +1,12 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        sec = 0
+        for j in range(i+1,len(prices)):
+            sec += 1
+            if prices[i] > prices[j]:  
+                break
+        answer.append(sec)
+    return answer
+
+# 실패. o(n**2) -> x
