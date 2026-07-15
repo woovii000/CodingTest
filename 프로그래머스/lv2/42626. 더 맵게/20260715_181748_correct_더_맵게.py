860d998503c8778195b37285d2ee4ea20b687725
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-15 18:17:49 +0900

    [ALG] correct | 프로그래머스 | lv2 | 더 맵게 | Python3 | 14번째 시도

---
+++ 프로그래머스/lv2/42626. 더 맵게/20260715_181748_correct_더_맵게.py
@@ -0,0 +1,18 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    # 힙 -> 자동 정렬
+    heapq.heapify(scoville) # list -> heap
+    while len(scoville) != 1:
+        if scoville[0] >= K:
+            return answer
+        
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville,first+second*2)
+        answer += 1
+        
+    if scoville[0]<K:
+        return -1
+    return answer
