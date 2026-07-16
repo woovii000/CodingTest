9932e8b2f31f1c4f8bc2ffa396d3f392226f5205
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-16 18:39:54 +0900

    [ALG] run | 프로그래머스 | lv2 | 다리를 지나는 트럭 | Python3 | 20번째 시도

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/20260716_183953_run_다리를_지나는_트럭.py
@@ -0,0 +1,20 @@
+def solution(bridge_length, weight, truck_weights):
+    in_b = []
+    count, idx, now_weight = 0, 0, 0
+
+    while () :
+        count += 1 # 경과시간+1
+
+        for truck in in_b:
+            truck[1] += 1 # 다리 위 모든 트럭 경과 시간+1
+
+        # 다리 위에 트럭이 있는지 && 다리 위 맨 앞 트럭이 경과 시간이 지났는지
+        if idx < len(in_b) and in_b[idx][1] >= bridge_length:
+            now_weight -= in_b[idx][0] # 지나간 트럭의 무게 줄이기
+
+        # 대기 중인 트럭이 있는지 && 현재 다리 위 무게 + 새로 넣을 트럭 무게 합 <= 제한 무게
+        if idx < len(truck_weights) and now_weight + bridge_length[idx] <= weight:
+            in_b.append([bridge_length[idx], 0])
+            now_weigtht += bridge_length[idx]
+
+    return count 
