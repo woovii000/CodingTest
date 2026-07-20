320d5b50a381b51d42ae7c103aa45ab387ac3bdd
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-20 18:55:46 +0900

    [ALG] run | 프로그래머스 | lv2 | 소수 찾기 | Python3 | 8번째 시도

---
+++ 프로그래머스/lv2/42839. 소수 찾기/20260720_185545_run_소수_찾기.py
@@ -0,0 +1,36 @@
+def solution(numbers):
+    # 방법1. 만들 수 있는 경우의 수 찾고 필터링*
+    split = list(numbers) # 문자열 list화
+    n = len(split) # 문자들의 개수
+    
+    used = [False] * n # **
+    all_num = set() # 조합 숫자
+    
+    def dfs(path):
+        if path and int(path) not in all_num:
+            all_num.add(int(path))
+        
+        for i in range(n):
+            if used[i]:
+                continue
+            used[i] = True
+            dfs(path + split[i])
+            used[i] = False
+        
+    dfs("")
+    count = 0 # 소수 개수
+    for num in all_num:
+        if num < 2:
+            continue
+        
+        is_prime = True
+        
+        for i in range(2, int(num**0.5)+1):
+            if num%i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            count += 1    
+    return count
+            
+    
