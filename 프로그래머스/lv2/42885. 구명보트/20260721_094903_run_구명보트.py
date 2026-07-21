c577e8f4dfeb7bcc1cf58a4e631f9c6b02a195a1
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-21 09:49:05 +0900

    [ALG] run | 프로그래머스 | lv2 | 구명보트 | Python3 | 5번째 시도

---
+++ 프로그래머스/lv2/42885. 구명보트/20260721_094903_run_구명보트.py
@@ -0,0 +1,21 @@
+def solution(people, limit):
+    # 2pointer?
+    people = sorted(people) # 오름차순 정렬
+    count = 0 # 옮긴 횟수
+    left = 0 # 정렬 후 맨앞 idx
+    right = len(people)-1 # 정렬 후 맨뒤 idx
+    while left != right:
+        current = 0 
+        current += people[right] 
+        # 최대 탑승인원: 2명
+        if people[right] + people[left] > limit:
+            right -= 1
+            count += 1
+            break
+        else:
+            right -= 1
+            left += 1
+            count += 1
+            break
+                
+    return count
