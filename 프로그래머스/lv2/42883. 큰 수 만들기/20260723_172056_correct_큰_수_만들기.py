194c40c6268b26c1354a69cf2589ed2282d02213
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-23 17:20:57 +0900

    [ALG] correct | 프로그래머스 | lv2 | 큰 수 만들기 | Python3 | 10번째 시도

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/20260723_172056_correct_큰_수_만들기.py
@@ -0,0 +1,15 @@
+def solution(number, k):
+    # 가장 큰수를 만들려면, 가장 작은 수를 빼면되지않나?
+    # 맨 앞에서 가장 작은 수를 먼저 빼는 방식* x
+    # -> 스택() + 다음 숫자가 나보다 큰가?
+    num = []
+    
+    for n in number:
+        while num and k > 0 and num[-1] < n:
+            num.pop()
+            k -= 1
+        num.append(n)
+        
+    if k > 0:
+        num = num[:-k]
+    return ''.join(num)
