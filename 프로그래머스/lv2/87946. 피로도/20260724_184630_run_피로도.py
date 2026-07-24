9a7f099648f4e06b75d03f63e21b176da4191374
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-24 18:46:32 +0900

    [ALG] run | 프로그래머스 | lv2 | 피로도 | Python3 | 5번째 시도

---
+++ 프로그래머스/lv2/87946. 피로도/20260724_184630_run_피로도.py
@@ -0,0 +1,15 @@
+def solution(k, dungeons):
+    answer = 0
+    visited = [False]*len(dungeons)
+    def gone(heal, count):
+        nonlocal answer
+        answer = max(answer, count)
+        for i in range(len(dungeons)):
+            need, lose = dungeons[i]
+            
+            if not visited[i] and heal >= need:
+                visited[i] = True
+                def gone(heal-lose, count+1)
+                visited[i]=False
+    gone(k,0)
+    return answer
