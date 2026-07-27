ee4758f29d596d5239d7a4bf6fa66d6647bbdf30
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-27 14:23:18 +0900

    [ALG] run | 프로그래머스 | lv2 | 큰 수 만들기 | Python3 | 16번째 시도

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/20260727_142317_run_큰_수_만들기.py
@@ -0,0 +1,15 @@
+def solution(number, k):
+    answer = list() # 만들 숫자
+    n = len(number)
+
+    for c in number:
+        while k>0:
+            if answer and int(answer[-1]) < int(c):
+                answer.pop()
+                k -= 1
+        answer.append(c)
+    
+    if k > 0:
+        answer = answer[:-k]
+        
+    return ''.join(answer)
