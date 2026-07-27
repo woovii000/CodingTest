f58bb5bf29e80dfb3cf633bad0b3c887feae9baa
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-27 18:16:42 +0900

    [ALG] run | 프로그래머스 | lv2 | 모음 사전 | Python3 | 34번째 시도

---
+++ 프로그래머스/lv2/84512. 모음 사전/20260727_181641_run_모음_사전.py
@@ -0,0 +1,20 @@
+def solution(word):
+    answer = 0
+    vowel = ['A', 'E', 'I', 'O', 'U']
+    w = list()
+    fin = False
+    def dfs(start):
+        for v in vowel:
+            if fin:
+                return
+            if ''.join(start) == word:
+                fin = True
+                
+            start.append(v)
+            answer += 1 
+            
+            if len(start) < 5:
+                dfs(start)
+        
+    dfs(w)
+    return answer
