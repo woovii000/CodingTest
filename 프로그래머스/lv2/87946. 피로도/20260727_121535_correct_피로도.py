16b10ea312eaed09e53dc9d1556c91b6a8a81f1f
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-27 12:15:37 +0900

    [ALG] correct | 프로그래머스 | lv2 | 피로도 | Python3 | 15번째 시도

---
+++ 프로그래머스/lv2/87946. 피로도/20260727_121535_correct_피로도.py
@@ -0,0 +1,17 @@
+def solution(k, dungeons):
+    answer = 0
+    visited = [False]*len(dungeons) # 방문 여부
+    def dfs(fatigue, count):
+        nonlocal answer # 전역변수
+        answer = max(answer, count) # 현재 방문 수 최댓값 확인
+        
+        for i in range(len(dungeons)): # 첫 방문여부로 돌기
+            need, use = dungeons[i] # 최소, 소모 피로도
+            # 방문x, k>=최소 피로도 -> 방문 가능
+            if not visited[i] and fatigue>=need:
+                visited[i] = True # 방문처리 + 다음 방문 진행
+                dfs(fatigue-use, count+1)
+                visited[i] = False
+    
+    dfs(k,0)
+    return answer
