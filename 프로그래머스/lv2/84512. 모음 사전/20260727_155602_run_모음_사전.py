92ef5be968fafe81b1e25db3c45c7ea7b5de3de1
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-27 15:56:03 +0900

    [ALG] run | 프로그래머스 | lv2 | 모음 사전 | Python3 | 9번째 시도

---
+++ 프로그래머스/lv2/84512. 모음 사전/20260727_155602_run_모음_사전.py
@@ -0,0 +1,17 @@
+def solution(word):                                      
+    answer = 0
+    vowel = ['A','E','I','O','U']
+    w = []
+    def dfs(make):
+        nonlocal answer
+        for i in vowel:
+            if make and ''.join(make) == word:
+                return
+            if len(make)==5:
+                return
+            make.append(i)
+            answer += 1
+            dfs(make)
+            make.pop(-1)
+    dfs(w)
+    return answer
