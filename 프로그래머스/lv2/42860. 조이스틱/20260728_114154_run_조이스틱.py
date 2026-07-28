8c296cdb1a5f4a31060a279a3d2f2640e7a542a5
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-28 11:41:56 +0900

    [ALG] run | 프로그래머스 | lv2 | 조이스틱 | Python3 | 14번째 시도

---
+++ 프로그래머스/lv2/42860. 조이스틱/20260728_114154_run_조이스틱.py
@@ -0,0 +1,28 @@
+def solution(name):
+    # 자리 -> [0]부터 각 자리 A부터 시작. len(alpha): 26
+    # 각자리 알파벳 이동 min(x, 26-x)
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    answer = 0
+    left, right = 0, 0
+    d = list() # 이동할 위치 list
+    
+    for c in name:
+        if c != 'A':
+            d.append(name.index(c)) # 이동할 위치 idx
+        x = alpha.index(c)
+        answer += min(x, 26-x) # 알파벳 별 최소 이동
+    
+    n = len(name)
+    drive = list()
+    
+    # 이동할 위치
+    for i in range(len(d)-1): # idx 0 -> not A
+        drive.append((d[i],d[i+1])) # (di, si)
+        
+    # 자리 별 최소 이동
+    for di, si in drive:
+        right += di-si
+        left += abs(-n+di-si)
+        
+    answer = min(left, right)
+    return answer
