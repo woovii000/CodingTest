1771f71a29d666e29323508e049d57202d7d743d
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-28 15:23:33 +0900

    [ALG] run | 프로그래머스 | lv2 | 조이스틱 | Python3 | 32번째 시도

---
+++ 프로그래머스/lv2/42860. 조이스틱/20260728_152331_run_조이스틱.py
@@ -0,0 +1,19 @@
+def solution(name):
+    change = 0
+    # 상하
+    for c in name: # 자리마다
+        diff = ord(c)-ord('A') # A 사이 간격
+        change += min(diff, 26-diff) # 둘 중 최소
+        
+    # 좌우
+    n = len(name)
+    move = n-1 # 오른쪽 한 방향 시, 최소
+    for i in range(n):
+        next_idx = i+1
+        while next_idx < n and name[i] == 'A':
+            next_idx += 1
+        
+    move = min(move, i*2 + n-next_idx) # 우 찍고, 좌 꺾기
+    move = min(move, (n-next_idx)*2 + i) # 좌 찍고, 우 꺾기
+    
+    return change + move
