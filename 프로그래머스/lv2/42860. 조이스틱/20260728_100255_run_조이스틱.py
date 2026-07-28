3459acc5b68c9bcd8493807b90952e2b20c98bb6
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-28 10:02:57 +0900

    [ALG] run | 프로그래머스 | lv2 | 조이스틱 | Python3 | 2번째 시도

---
+++ 프로그래머스/lv2/42860. 조이스틱/20260728_100255_run_조이스틱.py
@@ -0,0 +1,14 @@
+def solution(name):
+    # len: 26
+    alpha = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+    # 자리 -> [0]부터 각 자리 A부터 시작.
+    # 각자리 알파벳 이동 min(x, 26-x)
+    answer = 0
+    n = len(name)
+    for c in name:
+        x = alpha.index(c)
+        answer += min(x, 26-x) # 알파벳
+        #
+        # if c != 'A'
+        #     answer += min(x,n-x) # 자리 이동
+    return answer
