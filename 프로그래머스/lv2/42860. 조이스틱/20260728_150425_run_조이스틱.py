4c2ee5491c5a39f8216b983888bc3e7b06d2ed85
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-28 15:04:27 +0900

    [ALG] run | 프로그래머스 | lv2 | 조이스틱 | Python3 | 27번째 시도

---
+++ 프로그래머스/lv2/42860. 조이스틱/20260728_150425_run_조이스틱.py
@@ -0,0 +1,18 @@
+def solution(name):
+    change = 0
+    # 상하
+    for c in name:
+        diff = ord(c)-ord('A') # 바꿀 때, 클릭
+        change = min(diff, 26-diff) # 둘 중 최소
+        
+    # 좌우
+    n = len(name)
+    move = n-1 # 오른쪽 한 방향 시, 최소
+    for i in range(n):
+        next_idx = i+1
+        while next_idx < n and name[i] == 'A':
+            next_idx += 1
+        
+        move = min(move, i*2 + n-next_idx) # 오른쪽 찍고, 왼쪽 꺾기
+        move = min(move, (n-next_idx)*2 + i)# 오른쪽 찍고, 왼쪽 꺾기
+    return change + move
