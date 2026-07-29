7bbca50b99b5c65fc82591821542e128fdeaf147
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-29 16:43:39 +0900

    [ALG] run | 프로그래머스 | lv2 | 예상 대진표 | Python3 | 3번째 시도

---
+++ 프로그래머스/lv2/12985. 예상 대진표/20260729_164337_run_예상_대진표.py
@@ -0,0 +1,14 @@
+def solution(n,a,b):
+    index = 0
+    # n == 2**i -> 1~i회내로 a,b 만남
+    for i in (1,20):
+        if 2**i == n:
+            index = i
+    def dfs(i):
+        if i<=1:
+            return 1
+        if (a <= 2**(i-1) and b > 2**(i-1)) or (b <= 2**(i-1) and a > 2**(i-1)):
+            return i
+        else:
+            dfs(i-1)
+    dfs(index)
