87349994d4953ebc9c5c3afa113c37f142739d17
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-30 12:12:34 +0900

    [ALG] correct | 프로그래머스 | lv1 | 문자열 내 p와 y의 개수 | Python3 | 3번째 시도

---
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/20260730_121233_correct_문자열_내_p와_y의_개수.py
@@ -0,0 +1,10 @@
+def solution(s):
+    count = 0
+    for ch in s:
+        if ch == 'P' or ch == 'p':
+            count += 1
+        if ch == 'Y' or ch == 'y':
+            count -= 1
+    if not count:
+        return True
+    return False
