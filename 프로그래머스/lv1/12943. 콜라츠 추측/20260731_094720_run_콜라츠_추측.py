d34af94d6039da6bf7d8ac29b34337a66c6bd2fc
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-07-31 09:47:21 +0900

    [ALG] run | 프로그래머스 | lv1 | 콜라츠 추측 | Python3 | 3번째 시도

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/20260731_094720_run_콜라츠_추측.py
@@ -0,0 +1,12 @@
+def solution(num):
+    def collatz(n):
+        count = 0   
+        while n > 1:
+            n = n/2 if n%2==0 else 3*n+1
+            count += 1
+            if n == 1:
+                return count
+            elif count == 500 and num !=1:
+                return -1
+                 
+    collatz(num)
