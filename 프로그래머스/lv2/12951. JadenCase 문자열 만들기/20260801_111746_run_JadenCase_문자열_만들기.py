d6c2692ca49d768f3b69a4b0670aeeff938a804c
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-08-01 11:17:49 +0900

    [ALG] run | 프로그래머스 | lv2 | JadenCase 문자열 만들기 | Python3 | 1번째 시도

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/20260801_111746_run_JadenCase_문자열_만들기.py
@@ -0,0 +1,6 @@
+def solution(s):
+    words = s.split(' ')
+    for i in range(len(words)):
+        if words[i][0] > '0' and words[i][0] < '9':
+            words[i][0].upper()
+    return ''.join(words, ' ')
