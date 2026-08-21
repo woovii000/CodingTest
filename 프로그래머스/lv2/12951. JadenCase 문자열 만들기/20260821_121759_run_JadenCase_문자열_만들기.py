ba495d6c151d02d4da8b74f895f5f3b9c4cec800
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-08-21 12:18:00 +0900

    [ALG] run | 프로그래머스 | lv2 | JadenCase 문자열 만들기 | Python3 | 8번째 시도

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/20260821_121759_run_JadenCase_문자열_만들기.py
@@ -0,0 +1,14 @@
+def solution(s):
+    # 글자 나누기 + 붙이기
+    words = s.split(" ")
+    for ch in words:
+        for i in range(len(ch)):
+            # 맨 앞글자[0] 소문자, 대문자 => 대문자 변환
+            if i == 0 and ('a' < ch[i] or 'z' > ch[i]):
+                ch[i] = ch[i].upper()
+            # 이후 뒷글자[1~n] 대문자,소문자 -> 소문자
+            elif 'A' < ch[i] or 'Z' > ch[i]:
+                ch[i] = ch[i].lower()
+    
+    # 나눈 문자는 " "를 사이 문자로 해서 붙이고 출력
+    return ' '.join(words)
