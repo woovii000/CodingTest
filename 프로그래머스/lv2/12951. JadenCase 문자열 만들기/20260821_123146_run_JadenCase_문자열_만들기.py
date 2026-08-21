a382484b8e154830c23c3dbddd5680ad7d2d1320
YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-08-21 12:31:48 +0900

    [ALG] run | 프로그래머스 | lv2 | JadenCase 문자열 만들기 | Python3 | 10번째 시도

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/20260821_123146_run_JadenCase_문자열_만들기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    # 글자(str -> 불변) 나누기
+    words = s.split(" ")
+    
+    for idx in range(len(words)):
+        # 불변 -> list로 가변화
+        ch = list(words[idx])
+        for i in range(len(ch)):
+            # 맨 앞글자[0] 소문자, 대문자 => 대문자 변환
+            if i == 0 and ch[i].isalpha():
+                ch[i] = ch[i].upper()
+            # 이후 뒷글자[1~n] 대문자,소문자 -> 소문자
+            elif ch[i].isalpha():
+                ch[i] = ch[i].lower()
+            # 나눈 문자 붙이기
+            words[idx] = ''.join(ch)
+    return ''.join(words)
