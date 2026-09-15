82107b467ec5e1fed2a05c2f6313e27d4a733920
KANG YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-09-15 12:30:01 +0900

    [ALG] correct | 프로그래머스 | lv2 | H-Index | Python3 | 30번째 시도

---
+++ 프로그래머스/lv2/42747. H-Index/20260915_122959_correct_H-Index.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    # 65310
+    for i, citation in enumerate(citations,start=1):
+        if citation < i:
+            return i-1
+    
+    return len(citations)
+    
+            
