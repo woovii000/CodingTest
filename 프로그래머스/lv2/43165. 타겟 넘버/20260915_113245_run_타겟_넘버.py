eb847dc72678ce0aeeadf9145f3125c8b95b6c46
KANG YOUNG WOO <134402126+woovii000@users.noreply.github.com>
2026-09-15 11:32:47 +0900

    [ALG] run | 프로그래머스 | lv2 | 타겟 넘버 | Python3 | 14번째 시도

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/20260915_113245_run_타겟_넘버.py
@@ -0,0 +1,15 @@
+def solution(numbers, target):
+    answer = 0
+    def depth(index, result):
+        nonlocal answer
+        
+        if index = len(numbers):
+            if result == target:
+                answer += 1
+            return
+        
+        depth(index+1, result+numbers[index])
+        depth(index+1, result+numbers[index])
+        
+        
+    depth(0,0)
